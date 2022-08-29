9572d4daaa75142b0dd8d2d3de8ec387224bba0b
dvbarnett <darcybarnett@gmail.com>
2022-08-28 22:10:54 -0500

    Adding module work

---
+++ plots.js
@@ -0,0 +1,36 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+
+
+function optionChanged(newSample) {
+buildMetadata(newSample);
+buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var pairs = Object.entries(resultArray[0]);
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      var results = pairs.forEach(function (pair) {
+        PANEL.append('h6').text(pair[0] + ': ' + pair[1]);
+      });
+    });
+  }
